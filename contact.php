a23f783a6bf31f4ea48a372170a9eb5c41b2bb92
Mark O <mark.oconnor14@gmail.com>
2021-12-05 12:49:42 +0700

    add bootstrap and social proof to landing page

---
+++ contact.php
@@ -0,0 +1,27 @@
+<?php include_once "header.php"; ?>
+	<article>
+		<p>Please use the form below to talk to us. We will get back to you within 3 business days.</p>
+		<form class="form" action="">
+			<div>
+				<label for="">First Name</label>
+				<input type="text">
+			</div>
+			<div>
+				<label for="">Last Name</label>
+				<input type="text">
+			</div>
+			<div>
+				<label for="">Email</label>
+				<input type="text">
+			</div>
+			<div>
+				<label for="">Message</label>
+				<textarea></textarea>
+			</div>
+			<div>
+				<input type="submit">
+			</div>
+		</form>
+	</article>
+</main>
+<?php include_once "footer.php"; ?>
