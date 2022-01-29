f6907f092c126c48e6f37c198bc7fb17cc0288e7
Mark O <mark.oconnor14@gmail.com>
2022-01-29 09:34:56 +0700

    fix link in student functions

--- students/includes/student_functions.php
+++ students/includes/student_functions.php
@@ -4,6 +4,6 @@ function editStudentSignUpInfo($conn, $studentUid, $studentEmail, $studentPasswo
     $signup_student_query = "UPDATE `students` SET `studentsEmail`='".$studentEmail."',`studentsPassword`='".$studentPassword."' WHERE `studentsId`='".$studentUid."';";
     $signup_student_result = mysqli_query($conn, $signup_student_query);
     var_dump($signup_student_query);
-    header("location: ../signup                 .php?error=success");
+    header("location: ../signup.php?error=success");
 	exit();
 }
